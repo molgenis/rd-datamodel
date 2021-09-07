2aef39dadafb180776b527e9893051fcc5be4005
davidruvolo51 <davidruvolo51@gmail.com>
2021-09-07 12:51:30 +0200

    uncommented line

--- dev/example/index.py
+++ dev/example/index.py
@@ -4,7 +4,7 @@
 #' CREATED: 2021-09-07
 #' MODIFIED: 2021-09-07
 #' PURPOSE: test conversion
-#' STATUS: in.progress
+#' STATUS: working
 #' PACKAGES: NA
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
@@ -12,12 +12,5 @@
 from src.convert.convert import Convert
 c = Convert(file = 'dev/example/birddata.yml')
 c.convert()
-# c.write(format = 'xlsx', outDir = 'dev/example/model/')
+c.write(format = 'xlsx', outDir = 'dev/example/model/')
 c.write_schema(path = 'dev/example/model/birddata_schema.md')
-
-
-data = [
-    {'id': 'aaa', 'value': 124},
-    {'id': 'bbb', 'value': 163, 'type': 'xyz'},
-    {'id': 'ccc', 'value': 163},
-] 
