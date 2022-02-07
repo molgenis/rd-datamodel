4dc618d89156177e278b58fe6ea077a34a9a7dbd
davidruvolo51 <davidruvolo51@gmail.com>
2022-02-07 15:13:27 +0100

    added refresh setup script

--- dist/setup.sh
+++ dist/setup.sh
@@ -18,29 +18,35 @@
 
 # ~ 2 ~
 # Import URDM and assets
+
+# <!--- start: listEmxFiles --->
 mcmd import -p dist/umdm.xlsx
-mcmd import -p dist/umdm_lookups_anatomicalSource.csv
-mcmd import -p dist/umdm_lookups_ancestry.csv
-mcmd import -p dist/umdm_lookups_biospecimenType.csv
-mcmd import -p dist/umdm_lookups_country.csv
-mcmd import -p dist/umdm_lookups_dataUseModifiers.csv
-mcmd import -p dist/umdm_lookups_dataUsePermissions.csv
 mcmd import -p dist/umdm_lookups_diagnosisConfirmationStatuses.csv
-mcmd import -p dist/umdm_lookups_diseases.csv
-mcmd import -p dist/umdm_lookups_fileStatus.csv
-mcmd import -p dist/umdm_lookups_genomeAccessions.csv
-mcmd import -p dist/umdm_lookups_genotypicSex.csv
+mcmd import -p dist/umdm_lookups_country.csv
+mcmd import -p dist/umdm_lookups_genderIdentity.csv
 mcmd import -p dist/umdm_lookups_inclusionCriteria.csv
-mcmd import -p dist/umdm_lookups_ngsKits.csv
-mcmd import -p dist/umdm_lookups_pathologicalState.csv
-mcmd import -p dist/umdm_lookups_phenotype.csv
-mcmd import -p dist/umdm_lookups_phenotypicSex.csv
-mcmd import -p dist/umdm_lookups_samplingReason.csv
-mcmd import -p dist/umdm_lookups_sequencingInstrumentModels.csv
 mcmd import -p dist/umdm_lookups_sequencingMethods.csv
+mcmd import -p dist/umdm_lookups_ancestry.csv
+mcmd import -p dist/umdm_lookups_fileStatus.csv
+mcmd import -p dist/umdm_lookups_samplingReason.csv
+mcmd import -p dist/umdm_lookups_ngsKits.csv
 mcmd import -p dist/umdm_lookups_sequencingPlatform.csv
-mcmd import -p dist/umdm_lookups_studyStatus.csv
+mcmd import -p dist/umdm_lookups_phenotype.csv
+mcmd import -p dist/umdm_lookups_dataUseModifiers.csv
 mcmd import -p dist/umdm_lookups_subjectStatus.csv
+mcmd import -p dist/umdm_lookups_studyStatus.csv
+mcmd import -p dist/umdm_lookups_sequencingInstrumentModels.csv
+mcmd import -p dist/umdm_lookups_pathologicalState.csv
+mcmd import -p dist/umdm_lookups_genderAtBirth.csv
+mcmd import -p dist/umdm_lookups_genomeAccessions.csv
+mcmd import -p dist/umdm_lookups_diseases.csv
+mcmd import -p dist/umdm_lookups_biospecimenType.csv
+mcmd import -p dist/umdm_lookups_biospecimenUsability.csv
+mcmd import -p dist/umdm_lookups_anatomicalSource.csv
+mcmd import -p dist/umdm_lookups_dataUsePermissions.csv
+mcmd import -p dist/umdm_lookups_genotypicSex.csv
+# <!--- end: listEmxFiles --->
+
 
 # Optional: create demo
 # mcmd import -p data/demo/umdm_demo.xlsx --as umdm
